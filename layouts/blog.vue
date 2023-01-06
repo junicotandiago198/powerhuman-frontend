1f296bceb3aff8526e3af4e380895908fef1962e
junicotandiago198 <junicotandiagoo21@gmail.com>
2023-01-06 11:06:13 +0700

    first setup nuxt js and module auth, dotenv

---
+++ layouts/blog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>My blog navigation bar here</div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+    export default {
+        layout: 'blog',
+        // OR
+        layout (context) {
+            return 'blog'
+        },
+        // middleware: ['auth'],
+    }
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Hello World <strong>Changed</strong></h1>
+</template>
+
+<script>
+export default {
+    name: 'AboutVue',
+    layout: 'blog',
+    // middleware: ['auth'],
+}
+</script>
